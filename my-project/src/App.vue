5a8635f24aad2fca054286fbe549e735971c9516
Giovanni El Baruqui <giovannielbaruqui@ufrrj.br>
2022-10-06 15:44:08 -0300

    adicionando os codigos

---
+++ my-project/src/App.vue
@@ -0,0 +1,211 @@
+<template>
+  
+  <!-- Card -->
+
+  <div class="grid grid-cols-3 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 mt-4 w-full">
+
+    <!-- 1 -->
+    <div class="flex flex-col items-center relative w-full bg-white rounded-lg overflow-hidden shadow hover:shadow-md">
+      <div class="flex items-center justify-between w-full border-b border-gray-200 px-10 py-4">
+        <div>
+          <div class="flex space-x-2 pt-1">
+            <p class="font-medium text-gray-800">Nome</p>
+            <span class="flex bg-green-100 rounded-full text-green-800 text-xs px-2 py-1 leading-none">admin</span>
+          </div>
+          <p class="text-sm text-gray-600">Texto</p>
+        </div>
+        <img class="flex-none w-14 h-14 rounded-full object-cover" src="public\foto.jpeg">
+      </div>
+      <div class="w-full divide-x flex ">
+        <div class="flex flex-1 w-0 p-3">
+          <a href="#" class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-2 text-sm font-medium text-gray-700 hover:text-gray-500 space-x-1.5">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+            <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
+            <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />  
+            </svg>
+             <aspan>Email</aspan>
+          </a>
+        </div>
+        <div class="flex flex-1 w-0 p-3">
+          <a href="#" class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-2 text-sm font-medium text-gray-700 hover:text-gray-500 space-x-1.5">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+          <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clip-rule="evenodd" />
+        </svg>
+          <span>Call</span>
+        </a>
+        </div>
+      </div>
+    </div>
+
+    <!-- 2 -->
+
+    <div class="flex flex-col items-center relative w-full bg-white rounded-lg overflow-hidden shadow hover:shadow-md">
+      <div class="flex items-center justify-between w-full border-b border-gray-200 px-10 py-4">
+        <div>
+          <div class="flex space-x-2 pt-1">
+            <p class="font-medium text-gray-800">Nome</p>
+            <span class="flex bg-green-100 rounded-full text-green-800 text-xs px-2 py-1 leading-none">admin</span>
+          </div>
+          <p class="text-sm text-gray-600">Texto</p>
+        </div>
+        <img class="flex-none w-14 h-14 rounded-full object-cover" src="public\foto.jpeg">
+      </div>
+      <div class="w-full divide-x flex ">
+        <div class="flex flex-1 w-0 p-3">
+          <a href="#" class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-2 text-sm font-medium text-gray-700 hover:text-gray-500 space-x-1.5">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+            <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
+            <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />  
+            </svg>
+             <aspan>Email</aspan>
+          </a>
+        </div>
+        <div class="flex flex-1 w-0 p-3">
+          <a href="#" class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-2 text-sm font-medium text-gray-700 hover:text-gray-500 space-x-1.5">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+          <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clip-rule="evenodd" />
+        </svg>
+          <span>Call</span>
+        </a>
+        </div>
+      </div>
+    </div>
+    <!-- 3 -->
+
+    <div class="flex flex-col items-center relative w-full bg-white rounded-lg overflow-hidden shadow hover:shadow-md">
+      <div class="flex items-center justify-between w-full border-b border-gray-200 px-10 py-4">
+        <div>
+          <div class="flex space-x-2 pt-1">
+            <p class="font-medium text-gray-800">Nome</p>
+            <span class="flex bg-green-100 rounded-full text-green-800 text-xs px-2 py-1 leading-none">admin</span>
+          </div>
+          <p class="text-sm text-gray-600">Texto</p>
+        </div>
+        <img class="flex-none w-14 h-14 rounded-full object-cover" src="public\foto.jpeg">
+      </div>
+      <div class="w-full divide-x flex ">
+        <div class="flex flex-1 w-0 p-3">
+          <a href="#" class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-2 text-sm font-medium text-gray-700 hover:text-gray-500 space-x-1.5">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+            <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
+            <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />  
+            </svg>
+             <aspan>Email</aspan>
+          </a>
+        </div>
+        <div class="flex flex-1 w-0 p-3">
+          <a href="#" class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-2 text-sm font-medium text-gray-700 hover:text-gray-500 space-x-1.5">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+          <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clip-rule="evenodd" />
+        </svg>
+          <span>Call</span>
+        </a>
+        </div>
+      </div>
+    </div>
+
+    <!-- 4 -->
+
+    <div class="flex flex-col items-center relative w-full bg-white rounded-lg overflow-hidden shadow hover:shadow-md">
+      <div class="flex items-center justify-between w-full border-b border-gray-200 px-10 py-4">
+        <div>
+          <div class="flex space-x-2 pt-1">
+            <p class="font-medium text-gray-800">Nome</p>
+            <span class="flex bg-yellow-100 rounded-full text-yellow-600 text-xs px-2 py-1 leading-none">user</span>
+          </div>
+          <p class="text-sm text-gray-600">Texto</p>
+        </div>
+        <img class="flex-none w-14 h-14 rounded-full object-cover" src="public\foto.jpeg">
+      </div>
+      <div class="w-full divide-x flex ">
+        <div class="flex flex-1 w-0 p-3">
+          <a href="#" class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-2 text-sm font-medium text-gray-700 hover:text-gray-500 space-x-1.5">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+            <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
+            <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />  
+            </svg>
+             <aspan>Email</aspan>
+          </a>
+        </div>
+        <div class="flex flex-1 w-0 p-3">
+          <a href="#" class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-2 text-sm font-medium text-gray-700 hover:text-gray-500 space-x-1.5">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+          <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clip-rule="evenodd" />
+        </svg>
+          <span>Call</span>
+        </a>
+        </div>
+      </div>
+    </div>
+
+    <!-- 5 -->
+
+    <div class="flex flex-col items-center relative w-full bg-white rounded-lg overflow-hidden shadow hover:shadow-md">
+      <div class="flex items-center justify-between w-full border-b border-gray-200 px-10 py-4">
+        <div>
+          <div class="flex space-x-2 pt-1">
+            <p class="font-medium text-gray-800">Nome</p>
+            <span class="flex bg-green-100 rounded-full text-green-800 text-xs px-2 py-1 leading-none">admin</span>
+          </div>
+          <p class="text-sm text-gray-600">Texto</p>
+        </div>
+        <img class="flex-none w-14 h-14 rounded-full object-cover" src="public\foto.jpeg">
+      </div>
+      <div class="w-full divide-x flex ">
+        <div class="flex flex-1 w-0 p-3">
+          <a href="#" class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-2 text-sm font-medium text-gray-700 hover:text-gray-500 space-x-1.5">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+            <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
+            <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />  
+            </svg>
+             <aspan>Email</aspan>
+          </a>
+        </div>
+        <div class="flex flex-1 w-0 p-3">
+          <a href="#" class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-2 text-sm font-medium text-gray-700 hover:text-gray-500 space-x-1.5">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+          <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clip-rule="evenodd" />
+        </svg>
+          <span>Call</span>
+        </a>
+        </div>
+      </div>
+    </div>
+
+    <!-- 6 -->
+
+    <div class="flex flex-col items-center relative w-full bg-white rounded-lg overflow-hidden shadow hover:shadow-md">
+      <div class="flex items-center justify-between w-full border-b border-gray-200 px-10 py-4">
+        <div>
+          <div class="flex space-x-2 pt-1">
+            <p class="font-medium text-gray-800">Nome</p>
+            <span class="flex bg-green-100 rounded-full text-green-800 text-xs px-2 py-1 leading-none">admin</span>
+          </div>
+          <p class="text-sm text-gray-600">Texto</p>
+        </div>
+        <img class="flex-none w-14 h-14 rounded-full object-cover" src="public\foto.jpeg">
+      </div>
+      <div class="w-full divide-x flex ">
+        <div class="flex flex-1 w-0 p-3">
+          <a href="#" class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-2 text-sm font-medium text-gray-700 hover:text-gray-500 space-x-1.5">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+            <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
+            <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />  
+            </svg>
+             <aspan>Email</aspan>
+          </a>
+        </div>
+        <div class="flex flex-1 w-0 p-3">
+          <a href="#" class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-2 text-sm font-medium text-gray-700 hover:text-gray-500 space-x-1.5">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+          <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clip-rule="evenodd" />
+        </svg>
+          <span>Call</span>
+        </a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+</template>
